8cdd3a3da0f0a70c779fdb298899694d4a5334f3
rakeshjalagam <rakesh.jalagam776@gmail.com>
2026-08-05 11:36:42 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+javascript
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Azure Web App via DevOps CI/CD!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
